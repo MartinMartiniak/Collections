dfe67bbf698b6497b2a57b5f3aa2865d177010a5
z19ista18 <martiniak.martin15@gmail.com>
2021-11-23 09:13:50 +0100

    Create a List and print what it consists of.

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,18 @@
 package com.appslab;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        List<Integer> numbers = new ArrayList<>();
+        numbers = Arrays.asList(1, 1, 1, 2, 2, 3, 4, 5);
+        if (numbers.isEmpty()) {
+            System.out.println("List je prázdny");
+        } else {
+            numbers.forEach(System.out::println);
+        }
     }
 }
