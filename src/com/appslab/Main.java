13151a2b6b175e8e208a2bd3aeba2fd91e9cf04c
Martin Martiniak <martiniak.martin15@gmail.com>
2021-11-23 19:05:49 +0100

    Edit the multiplication by length

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,26 @@
 package com.appslab;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        MultiplyByLength multiplyByLength = (List<Integer> list) -> {
+            List<Integer> listOfEvenNumbers = new ArrayList<>();
+
+            list.stream().map(i->i*list.size()).forEach(listOfEvenNumbers::add);
+            System.out.println(listOfEvenNumbers);
+        };
+
+        List<Integer> Array1 = new ArrayList<>();
+        Array1.add(2);
+        Array1.add(3);
+        Array1.add(1);
+        Array1.add(0);
+
+        multiplyByLength.multipList(Array1); //don't mind the pun :D
+    }
+    interface MultiplyByLength{
+      void multipList(List<Integer> list);
     }
 }
