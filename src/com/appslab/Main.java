cec1ab4008e03e0a24e8c4c3effed437a4598192
Martin Martiniak <martiniak.martin15@gmail.com>
2021-11-24 17:29:23 +0100

    Create a stream which adds up all the budget of people in People list.

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,49 @@
 package com.appslab;
 
+import java.util.stream.Stream;
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        GetWholeBudget getWholeBudget = (List<People> peopleList) -> {
+            Integer budget = peopleList.stream().map(People::getBudget);
+            System.out.println(budget);
+        };
+        People person1 = new People("John", 21, 23000 );
+        People person2 = new People("Steve", 32, 40000);
+        People person3 = new People("Martin", 16, 2700);
+
+        List<People> peopleList = new ArrayList<>();
+
+        peopleList.add(person1);
+        peopleList.add(person2);
+        peopleList.add(person3);
+
+        getWholeBudget.wholeBudget(peopleList);
+    }
+    interface GetWholeBudget{
+        void wholeBudget(List<People> personList);
+    }
+}
+class People {
+    private String name;
+    private int age;
+    private int budget;
+
+    public People(String name, int age, int budget) {
+        this.name = name;
+        this.age = age;
+        this.budget = budget;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public Integer getBudget() {
+        return budget;
+    }
+
+    public int getAge() {
+        return age;
     }
 }
