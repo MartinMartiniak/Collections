abe6e1dbffdc78b6bc9b0467b114a07ec367a074
Martin Martiniak <martiniak.martin15@gmail.com>
2021-11-24 20:33:20 +0100

    Create classes and print the instances of via abstract class, so I don't duplicate code.

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,8 +3,8 @@ package com.appslab;
 public class Main {
 
         public static void main(String[] args) {
-            Teacher teacher = new Teacher(2500,420);
-            Programmer programmer = new Programmer(5000,690);
+            Teacher teacher = new Teacher(900,300);
+            Programmer programmer = new Programmer(1700,200);
 
             teacher.getInfo();
             programmer.getInfo();
