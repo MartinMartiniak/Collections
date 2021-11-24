babeb456c66ea7369a03b14cc16196834b6640dc
Martin Martiniak <martiniak.martin15@gmail.com>
2021-11-24 18:42:32 +0100

    Create classes and print the instances of them.

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -2,7 +2,11 @@ package com.appslab;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        public static void main(String[] args) {
+            Teacher teacher = new Teacher(2500,420);
+            Programmer programmer = new Programmer(5000,690);
+
+            teacher.getInfo();
+            programmer.getInfo();
+        }
     }
-}
---
+++ src/com/appslab/Teacher.java
@@ -0,0 +1,24 @@
+package com.appslab;
+
+
+public class Teacher {
+
+    private float salary;
+    private int bonus;
+
+    public Teacher(float salary, int bonus) {
+        this.salary = salary;
+        this.bonus = bonus;
+    }
+    public float getSalary() {
+        return salary;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+    public void getInfo()
+    {
+        System.out.println("Teacher's salary is "+salary+ " and bonus is "+bonus);
+    }
+}
